a447bbfbd354b636b1e9aa850074f39c4358b51e
ksyv <viais.kevin@hotmail.fr>
2024-11-29 12:04:00 +0100

    add island perimeter

---
+++ island_perimeter/0-island_perimeter.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+
+def island_perimeter(grid):
+    if grid is None or len(grid) == 0:
+        return 0
+    rows = len(grid)
+    columns = len(grid[0])
+    perimeter = 0
+    for row in range(rows):
+        for column in range(columns):
+            if grid[row][column] == 1:
+                if row == 0 or grid[row - 1][column] == 0:
+                    perimeter += 1
+                if row == rows - 1 or grid[row + 1][column] == 0:
+                    perimeter += 1
+                if column == 0 or grid[row][column - 1] == 0:
+                    perimeter += 1
+                if column == columns - 1 or grid[row][column + 1] == 0:
+                    perimeter += 1
+    return perimeter
