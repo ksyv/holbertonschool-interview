f0810a025a02c557dbb301c2612e92d22c8dfff6
ksyv <viais.kevin@hotmail.fr>
2024-11-29 11:30:09 +0100

    delete test

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -22,11 +22,3 @@ def canUnlockAll(boxes):
                 queue.append(key)
     return all(visited)
 
-boxes = [[1], [2], [3], [4], []]
-print(canUnlockAll(boxes))
-
-boxes = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
-print(canUnlockAll(boxes))
-
-boxes = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
-print(canUnlockAll(boxes))
