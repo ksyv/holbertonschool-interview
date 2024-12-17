5a3658787d1c6df90e7ae6207ce16132838f0dc6
ksyv <viais.kevin@hotmail.fr>
2024-12-17 13:22:35 +0100

    fix pep8

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python3
 """
-In a text file, there is a single character H. Your text editor can execute only two operations in this file: Copy All and Paste.
-Given a number n, write a method that calculates the fewest number of operations needed to result in exactly n H characters in the file.
+In a text file, there is a single character H.
+Your text editor can execute only two operations in this file:
+Copy All and Paste.
+Given a number n, write a method that calculates the fewest
+number of operations needed to result in exactly n H characters in the file.
 Be smart about how you utilize the memory!
 Prototype: def minOperations(n)
 Returns an integer
 If n is impossible to achieve, return 0
 """
+
+
 def minOperations(n):
     if n > 1:
         nboperations = 0
