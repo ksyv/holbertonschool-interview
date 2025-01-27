eb6a6fb02f4ada772b600f82dfb4192f147fcb46
ksyv <viais.kevin@hotmail.fr>
2025-01-27 14:45:04 +0100

    fix pycode

--- utf8_validation/0-validate_utf8.py
+++ utf8_validation/0-validate_utf8.py
@@ -3,18 +3,19 @@
 Module for test if data's byte sequence is UTF8 compatible.
 """
 
+
 def validUTF8(data):
     index = 0
     while index < len(data):
         byte = data[index]
 
         if (byte >> 7) == 1:
-            
+
             one_counter = 0
             while (byte >> (7 - one_counter)) & 1 == 1:
                 one_counter += 1
 
-            if one_counter == 0 or one_counter > 4 or index + one_counter > len(data):
+            if one_counter > 4 or index + one_counter > len(data):
                 return False
 
             for i in range(1, one_counter):
